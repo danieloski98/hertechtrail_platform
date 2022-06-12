bfcb25741870b5f9944c1ea5327edf5b27764341
= <=>
2022-06-12 16:32:09 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
